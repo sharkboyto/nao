76ab03a0e34367d62f41dd509d16556b5da0f9f8
sharkboyto <sharkboy_torino@yahoo.it>
2021-12-02 09:17:46 +0100

    fix version compatibility

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Nao (NVDA Advanced OCR) is an addon that improves the standard OCR capabilities that NVDA provides on modern Windows versions."""),
 	# version
-	"addon_version": "2021.1.03",
+	"addon_version": "2021.1.04",
 	# Author(s)
 	"addon_author": "Alessandro Albano<ale.albano1980@gmail.com>, Davide De Carne and Simone Dal Maso<simone.dalmaso@gmail.com>",
 	# URL for the add-on documentation support
@@ -33,9 +33,9 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": 2021.1,
+	"addon_minimumNVDAVersion": 2021.2,
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": 2021.1,
+	"addon_lastTestedNVDAVersion": 2021.3,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
