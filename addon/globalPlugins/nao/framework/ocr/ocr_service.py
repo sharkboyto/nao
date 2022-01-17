9421a6a7ce9c4b7ac99ea82c42fdc06145b9a49f
davidedec <davide.dec@gmail.com>
2022-01-17 16:58:26 +0100

    porting OCRService on Thread class and language aware recognition

--- addon/globalPlugins/nao/framework/ocr/ocr_service.py
+++ addon/globalPlugins/nao/framework/ocr/ocr_service.py
@@ -1,7 +1,7 @@
 #Nao (NVDA Advanced OCR) is an addon that improves the standard OCR capabilities that NVDA provides on modern Windows versions.
 #This file is covered by the GNU General Public License.
 #See the file COPYING for more details.
-#Last update 2022-01-07
+#Last update 2022-01-17
 #Copyright (C) 2021 Alessandro Albano, Davide De Carne and Simone Dal Maso
 
 import weakref
@@ -10,7 +10,7 @@ import threading
 import winGDI
 import wx
 from contentRecog import uwpOcr, recogUi
-
+from .. threading import Thread, ProgramTerminateEvent
 from .. import language
 
 language.initTranslation()
@@ -29,6 +29,17 @@ class OCRService:
 	def is_uwp_ocr_available():
 		return winVersion.isUwpOcrAvailable()
 
+	class QueueItem:
+		def __init__(self, bitmap=None, pixels=None, width=None, height=None, language=None, on_recognize_result=None):
+			if not language: language = uwpOcr.getConfigLanguage()
+			if bitmap and (width is None or height is None): width, height = bitmap.Size.Get()
+			self.bitmap = bitmap
+			self.pixels = pixels
+			self.width = width
+			self.height = height
+			self.language = language
+			self.on_recognize_result = on_recognize_result
+
 	@classmethod
 	def _instance(cls):
 		return None
@@ -45,75 +56,72 @@ class OCRService:
 		self._thread = None
 		self._queue = []
 		self._queue_lock = threading.Lock()
-		self._queue_push = threading.Event()
+		self._queue_push = ProgramTerminateEvent()
 		self._pixels = None
 		self._pixels_size = [0, 0]
 		self._fake_recog = FakeRecog()
 
 	def push_bitmap(self, bitmap, on_recognize_result):
 		if bitmap and on_recognize_result:
-			width, height = bitmap.Size.Get()
-			self._queue_lock.acquire()
-			self._queue.append([bitmap, None, width, height, on_recognize_result])
-			self._queue_push.set()
-			self._thread_start()
-			self._queue_lock.release()
+			self._push(OCRService.QueueItem(bitmap=bitmap, on_recognize_result=on_recognize_result))
 		elif on_recognize_result:
 			on_recognize_result(ValueError("Invalid bitmap"))
 
 	def push_pixels(self, pixels, width, height, on_recognize_result):
 		if pixels and on_recognize_result:
+			self._push(OCRService.QueueItem(pixels=pixels, width=width, height=height, on_recognize_result=on_recognize_result))
+
+	def _thread_start(self):
+		if not self._thread:
+			self._thread = Thread(target=self._threadproc)
+			self._thread.start()
+
+	def _push(self, item):
+		if item:
 			self._queue_lock.acquire()
-			self._queue.append([None, pixels, width, height, on_recognize_result])
+			self._queue.append(item)
 			self._queue_push.set()
 			self._thread_start()
 			self._queue_lock.release()
 
-	def _thread_start(self):
-		if not self._thread:
-			self._thread = threading.Thread(target = self._threadproc)
-			self._thread.setDaemon(True)
-			self._thread.start()
-
-	def _recognize(self, bmp, pixels, width, height, on_recognize_result):
-		if not pixels and bmp:
-			if not self._pixels or self._pixels_size[0] != width or self._pixels_size[1] != height:
+	def _recognize(self, item):
+		if not item.pixels and item.bitmap:
+			if not self._pixels or self._pixels_size[0] != item.width or self._pixels_size[1] != item.height:
 				try:
-					self._pixels = (winGDI.RGBQUAD*width*height)()
-					self._pixels_size = [width, height]
+					self._pixels = (winGDI.RGBQUAD*item.width*item.height)()
+					self._pixels_size = [item.width, item.height]
 				except MemoryError as e:
-					on_recognize_result(e)
+					item.on_recognize_result(e)
 					self._pixels = None
 			if self._pixels:
-				bmp.CopyToBuffer(self._pixels, format=wx.BitmapBufferFormat_ARGB32)
-				pixels = self._pixels
-		if pixels:
-			recognizer = uwpOcr.UwpOcr()
+				item.bitmap.CopyToBuffer(self._pixels, format=wx.BitmapBufferFormat_ARGB32)
+				item.pixels = self._pixels
+		if item.pixels:
+			recognizer = uwpOcr.UwpOcr(language=item.language)
 			try:
-				imgInfo = recogUi.RecogImageInfo.createFromRecognizer(0, 0, width, height, recognizer)
+				imgInfo = recogUi.RecogImageInfo.createFromRecognizer(0, 0, item.width, item.height, recognizer)
 			except ValueError as e:
-				on_recognize_result(e)
+				item.on_recognize_result(e)
 				return
-			event = threading.Event()
+			event = ProgramTerminateEvent()
 			def h(result):
-				on_recognize_result(result)
+				item.on_recognize_result(result)
 				event.set()
 			try:
-				recognizer.recognize(pixels, imgInfo, h)
+				recognizer.recognize(item.pixels, imgInfo, h)
 				event.wait()
 			except Exception as e:
-				on_recognize_result(e)
+				item.on_recognize_result(e)
 
-	def _threadproc(self):
-		must_exit = False
+	def _threadproc(self, wait):
 		self._queue_lock.acquire()
-		while not must_exit:
-			while not recogUi._activeRecog and len(self._queue) > 0:
+		while not wait.must_terminate() and len(self._queue) > 0:
+			while not wait.must_terminate() and len(self._queue) > 0 and not recogUi._activeRecog:
 				recogUi._activeRecog = self._fake_recog
-				bmp, pixels, width, height, on_recognize_result = self._queue.pop(0)
+				item = self._queue.pop(0)
 				self._queue_lock.release()
-				if (bmp or pixels) and on_recognize_result:
-					self._recognize(bmp, pixels, width, height, on_recognize_result)
+				if (item.bitmap or item.pixels) and item.on_recognize_result:
+					self._recognize(item)
 				self._queue_lock.acquire()
 				recogUi._activeRecog = None
 				try:
@@ -123,12 +131,12 @@ class OCRService:
 				if self._fake_recog._cancelled:
 					break
 			self._queue_push.clear()
-			self._queue_lock.release()
-			self._queue_push.wait(timeout=5)
-			self._queue_lock.acquire()
+			if not wait.must_terminate():
+				self._queue_lock.release()
+				self._queue_push.wait(timeout=5)
+				self._queue_lock.acquire()
 			self._fake_recog._lock.acquire(blocking=False)
 			self._fake_recog._cancelled = False
-			must_exit = len(self._queue) == 0
 		self._thread = None
 		self._pixels = None
 		self._queue_lock.release()
